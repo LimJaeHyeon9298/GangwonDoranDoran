e0a776f64e968e7d8113ff9e60779db92f4a2738
LimJaeHyeon9298 <limjh9298@gmail.com>
2024-07-13 22:15:09 +0900

    [feat] customTabBar UI 구현

--- GangwonDoranDoran/GangwonDoranDoran/ViewController/HomeViewController.swift
+++ GangwonDoranDoran/GangwonDoranDoran/ViewController/HomeViewController.swift
@@ -13,9 +13,23 @@ import SnapKit
 
 class HomeViewController:UIViewController {
     
+    //    let customView = CustomTabView().then {
+    //        $0.backgroundColor = .blue
+    //
+    //    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        let customTabBar = CustomTabView()
+        customTabBar.frame = CGRect(x: 0, y: self.view.frame.height - 80, width: self.view.frame.width, height: 200)
+        customTabBar.backgroundColor = .clear
+        
+        self.view.addSubview(customTabBar)
+        
     }
     
 }
+    
